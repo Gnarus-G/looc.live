1e0eb10dae978b40894913842d39607ef2e33bbe
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2022-10-03 00:31:38 -0400

    refactor: a service to manage calls

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,11 @@
 import cuid from "cuid";
 import cors from "cors";
 import e from "express";
+import { CallsManager } from "./manage-calls";
 
 const app = e();
 
-type SDPAndCandidates = {
-  sdp: string;
-  type: string;
-  candidates: object[];
-};
-
-type RTCData = {
-  offer: SDPAndCandidates;
-  answer?: SDPAndCandidates;
-};
-
-const calls = new Map<string, RTCData>();
-
-const answerSubs = new Map<string, Array<(a: SDPAndCandidates) => void>>();
-const iceCandidateSubs = new Map<
-  string,
-  Array<(a: SDPAndCandidates) => void>
->();
+const calls = new CallsManager();
 
 app.use(cors());
 app.use(e.json());
@@ -29,103 +13,45 @@ app.use(e.json());
 app.post("/offer/:id", (req, res) => {
   const id = req.params.id ?? cuid();
   console.log("Offer", req.body);
-  calls.set(id, {
-    offer: {
-      ...req.body,
-      candidates: [],
-    },
-  });
-
-  res.json({
-    id,
-  });
+  calls.setOffer(id, req.body);
+  res.json({ id });
 });
 
 app.get("/offer/:id", (req, res) => {
-  const call = calls.get(req.params.id);
-
+  const call = calls.getOffer(req.params.id);
   if (!call) return res.status(400);
-
-  res.json({
-    sdp: call.offer.sdp,
-    type: call.offer.type,
-  });
+  res.json(call);
 });
 
 app.post("/answer/:id", (req, res) => {
   const id = req.params.id;
-  const call = calls.get(id);
-
+  const call = calls.getAnswer(id);
   if (!call) return res.status(400).json({});
-
   const answer = req.body;
-
   console.log("answering call", id, answer);
-
-  calls.set(id, {
-    ...call,
-    answer: {
-      ...answer,
-      candidates: [],
-    },
-  });
-
-  answerSubs.get(id)?.forEach((s) => s(answer));
-
-  res.json({
-    id,
-  });
+  res.json({ id });
 });
 
 app.post("/offer/:id/candidate", (req, res) => {
   const id = req.params.id;
-  const call = calls.get(id);
-  if (!call) return res.status(400);
-  call.offer.candidates.push(req.body);
-  console.log("setting offer candidate", call);
-  iceCandidateSubs.get(id)?.forEach((s) => s(call.offer));
+  calls.setOfferCandidate(id, req.body);
+  console.log("setting offer candidate for call", id);
   res.status(204);
 });
 
 app.post("/answer/:id/candidate", (req, res) => {
   const id = req.params.id;
-  const call = calls.get(id);
-  if (!call) return res.status(400);
-  const answer = call.answer;
-  console.log("setting answer candidate", call);
-  if (answer) {
-    answer.candidates.push(req.body);
-    iceCandidateSubs.get(id)?.forEach((s) => s(answer));
-  }
+  console.log("setting answer candidate for call", id);
+  calls.setAnswerCandidate(id, req.body);
   res.status(204);
 });
 
 app.get("/events/:id", (req, res) => {
   const callId = req.params.id;
-  let subs = answerSubs.get(callId);
-  if (!subs) {
-    subs = [];
-    answerSubs.set(callId, subs);
-  }
 
-  subs.push((a) => {
-    console.log("pushing answer", a);
-    /* res.write(`id: ${callId}\n`); */
-    res.write("event: answer\n");
-    res.write(`data: ${JSON.stringify(a)}\n\n`);
-  });
-
-  let iceSubs = iceCandidateSubs.get(callId);
-  if (!iceSubs) {
-    iceSubs = [];
-    iceCandidateSubs.set(callId, iceSubs);
-  }
-
-  iceSubs.push((sdpIc) => {
-    console.log("pushing ice candidate", sdpIc);
-    /* res.write(`id: ${callId}\n`); */
-    res.write(`event: ${sdpIc.type}Candidate\n`);
-    res.write(`data: ${JSON.stringify(sdpIc.candidates.at(-1))}\n\n`);
+  const unregisterListener = calls.registerEventListener(callId, (e) => {
+    res.write(`event: ${e.type}\n`);
+    res.write(`data: ${JSON.stringify(e.data)}\n\n`);
   });
 
   res.contentType("text/event-stream");
@@ -134,8 +60,7 @@ app.get("/events/:id", (req, res) => {
   });
 
   res.on("close", () => {
-    /* answerSubs.delete(callId); */
-    /* iceCandidateSubs.delete(callId); */
+    unregisterListener();
     res.end();
   });
 });
---
+++ server/manage-calls.ts
@@ -0,0 +1,80 @@
+interface SDPOffer {
+  sdp: string;
+  type: "offer";
+}
+
+interface SDPAnswer {
+  sdp: string;
+  type: "answer";
+}
+
+type ICECandidate = Record<string, unknown>;
+
+interface CallEvent {
+  type: "answer" | "offerCandidate" | "answerCandidate";
+  data: SDPAnswer | ICECandidate;
+}
+
+type CallEventListener = (e: CallEvent) => void;
+
+export class CallsManager {
+  private offerMap = new Map<string, SDPOffer>();
+  private answerMap = new Map<string, SDPAnswer>();
+
+  private eventListeners = new Map<string, Set<CallEventListener>>();
+
+  setOffer(callId: string, sdp: SDPOffer) {
+    this.offerMap.set(callId, sdp);
+  }
+
+  getOffer(callId: string) {
+    return this.offerMap.get(callId);
+  }
+
+  setAnswer(callId: string, sdp: SDPAnswer) {
+    this.answerMap.set(callId, sdp);
+    this.eventListeners.get(callId)?.forEach((l) =>
+      l({
+        type: "answer",
+        data: sdp,
+      })
+    );
+  }
+
+  getAnswer(callId: string) {
+    return this.answerMap.get(callId);
+  }
+
+  setOfferCandidate(callId: string, c: ICECandidate) {
+    console.log("pushing an offer ice candidate", c);
+    this.eventListeners.get(callId)?.forEach((l) =>
+      l({
+        type: "offerCandidate",
+        data: c,
+      })
+    );
+  }
+
+  setAnswerCandidate(callId: string, c: ICECandidate) {
+    console.log("pushing an answer ice candidate", c);
+    this.eventListeners.get(callId)?.forEach((l) =>
+      l({
+        type: "answerCandidate",
+        data: c,
+      })
+    );
+  }
+
+  registerEventListener(callId: string, listener: CallEventListener) {
+    let listeners = this.eventListeners.get(callId);
+    if (!listeners) {
+      listeners = new Set();
+    }
+    listeners.add(listener);
+    this.eventListeners.set(callId, listeners);
+
+    return () => {
+      listeners?.delete(listener);
+    };
+  }
+}
