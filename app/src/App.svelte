50093464687bb10bab394535c7cfcdd7abf383a5
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-05-17 21:26:52 -0400

    chore: better styling for front page

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -3,10 +3,14 @@
 
   let userName: string;
   let showLooc = false;
+  let error = false;
 
   async function submitUserNameAndShowLooc() {
     if (userName) {
       showLooc = true;
+      error = false;
+    } else {
+      error = true;
     }
   }
 </script>
@@ -22,8 +26,9 @@
     />
     <form
       id="call-form"
-      class="flex flex-wrap items-center justify-center"
+      class="md:flex items-center justify-center"
       on:submit|preventDefault={submitUserNameAndShowLooc}
+      novalidate
     >
       <label class="block">
         <span
@@ -32,18 +37,20 @@
         >
         <input
           type="text"
-          class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+          class="peer mt-1 px-3 py-2 bg-white border shadow-sm {error
+            ? 'border-red-400'
+            : 'border-slate-300'} placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
           bind:value={userName}
           required
         />
-        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-          Please provide a username
-        </p>
+        {#if error}
+          <p class="mt-2 text-red-400 text-sm">Please provide a username</p>
+        {/if}
       </label>
       <input hidden type="submit" />
       <div class="flex justify-center gap-10 p-5">
         <button
-          class="rounded-2xl text-white px-2 py-1 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+          class="rounded text-white px-2 py-1 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none active:scale-95 focus:ring-violet-300"
           type="submit">Submit</button
         >
       </div>
