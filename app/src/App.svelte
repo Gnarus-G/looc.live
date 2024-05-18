c2716ba57748bb5a43088df69078cfec784c3ffc
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-05-18 13:05:21 -0400

    fix: styles

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -26,7 +26,7 @@
     />
     <form
       id="call-form"
-      class="md:flex items-center justify-center"
+      class="md:flex items-end justify-center"
       on:submit|preventDefault={submitUserNameAndShowLooc}
       novalidate
     >
@@ -37,7 +37,7 @@
         >
         <input
           type="text"
-          class="peer mt-1 px-3 py-2 bg-white border shadow-sm {error
+          class="peer mt-1 px-3 py-1 bg-white border shadow-sm {error
             ? 'border-red-400'
             : 'border-slate-300'} placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
           bind:value={userName}
@@ -48,7 +48,7 @@
         {/if}
       </label>
       <input hidden type="submit" />
-      <div class="flex justify-center gap-10 p-5">
+      <div class="flex justify-center gap-10 py-2 md:py-0 px-2">
         <button
           class="rounded text-white px-2 py-1 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none active:scale-95 focus:ring-violet-300"
           type="submit">Submit</button
